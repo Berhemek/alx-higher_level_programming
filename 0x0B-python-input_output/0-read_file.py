8f0c7da1ce8d92b585c76414bb09f54387e7e6c3
Berhe Mekonen <berhemek21@gmail.com>
2023-07-11 05:42:12 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,16 +3,7 @@
 
 
 def read_file(filename=""):
-    """ Function that reads from a file
+    """ Function that reads from a file """
 
-    Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
-
-    """
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end='')
