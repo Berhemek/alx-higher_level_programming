b12d46533669eb4a600bc98126269e82d541e0e2
Berhe <berhemek21@gmail.com>
2023-09-30 14:00:48 -0700

    Fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,17 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+url = "https://alx-intranet.hbtn.io/status"
 
-def fetcher():
-    """fetcher"""
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        data = response.read().decode('utf-8')
+
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type:", type(data))
+        print("\t- content:", data)
+except urllib.error.HTTPError as e:
+    print(f"Error: {e.code} {e.reason}")
+except urllib.error.URLError as e:
+    print(f"Error: {e.reason}")
 
-if __name__ == "__main__":
-    fetcher()
